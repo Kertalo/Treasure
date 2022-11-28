d66ec98f043c4d5fabce7086133fcc62f42548ff
Rogalik <koksharov-kir@yandex.ru>
2022-11-28 22:57:35 +0300

    add two buttons and two pages

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,4 +2,12 @@ class GameController < ApplicationController
   def main
 
   end
+
+  def loading
+
+  end
+
+  def change_labyrinth
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'game#main'
-  get '/deadend', to: 'quest#deadend'
+
+  get '/loading', to: 'game#loading'
+  get '/change_labyrinth', to: 'game#change_labyrinth'
 end
