9f1626a36a4342b84c4ff4a76ddd575ee5a06f35
Viktor <viter02@inbox.ru>
2022-12-09 17:17:48 +0300

    Starded developing registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_075944) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_141426) do
   create_table "player_status", force: :cascade do |t|
     t.string "user_id"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
