61d7ba3194955ad3feb3d400f19671f409f2af9f
Viktor <viter02@inbox.ru>
2022-12-18 11:10:25 +0300

    Change db to postgres

--- db/migrate/20221209172320_create_player_statuses.rb
+++
@@ -1,9 +0,0 @@
-class CreatePlayerStatuses < ActiveRecord::Migration[7.0]
-  def change
-    create_table :player_statuses do |t|
-      t.string :status, null: false
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20221211153432_drop_table_player_statuses.rb
+++
@@ -1,5 +0,0 @@
-class DropTablePlayerStatuses < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :player_statuses
-  end
-end
